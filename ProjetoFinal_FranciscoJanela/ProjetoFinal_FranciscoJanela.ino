dd3427f271ebad328d2417e140a547df0010bf6c
Francisco Janela <francisco.pinheiro.janela@gmail.com>
2020-06-07 11:44:10 -0300

    Bibliotecas e Variáveis do Projeto - TMP, DHT, LDR

--- ProjetoFinal_FranciscoJanela/ProjetoFinal_FranciscoJanela.ino
+++ ProjetoFinal_FranciscoJanela/ProjetoFinal_FranciscoJanela.ino
@@ -4,11 +4,44 @@
     @Autor: Francisco Janela - 1C
     @Professor: Fabio Ferraz Junior
  */
+ 
+////////////// Bibliotecas e Variáveis ///////////////
+// Liquid Crystal Display:
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
+// TMP 36:
+float TMPcelsius = 0.0;
+int TMPsensorValue;
+float TMPtensao;
 
+
+// DHT 22:
+#include "DHT.h"
+
+#define DHTPIN 7
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN,DHTTYPE);
+
+float DHTumidade;
+
+
+//LDR:
+int LDRsensorValue;
+float LDRtensao, LDRresistencia, LDRluminosidade;
+
+
+/////////////////// SETUP DO CÓDIGO /////////////////////
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //LCD:
+  //TMP 36:
+  //DHT 22:
+  //LDR:
 
 }
 
