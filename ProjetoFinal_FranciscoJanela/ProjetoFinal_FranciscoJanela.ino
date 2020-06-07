a839a3d32d29b770bcb1540e81136ada327d6723
Francisco Janela <francisco.pinheiro.janela@gmail.com>
2020-06-07 12:01:19 -0300

    Sub-rotinas com calibração

--- ProjetoFinal_FranciscoJanela/ProjetoFinal_FranciscoJanela.ino
+++ ProjetoFinal_FranciscoJanela/ProjetoFinal_FranciscoJanela.ino
@@ -38,6 +38,7 @@ float LDRtensao, LDRresistencia, LDRluminosidade;
 /////////////////// SETUP DO CÓDIGO /////////////////////
 void setup() {
   Serial.begin(9600);
+  analogReference(INTERNAL);
   //LCD:
   //TMP 36:
   //DHT 22:
@@ -45,6 +46,32 @@ void setup() {
 
 }
 
+///////////////// DEFININDO SUB-ROTINAS ///////////////////
+// Sub-rotina do TMP com calibração:
+float TMP_celsiusCalibrado() {
+  TMPsensorValue = analogRead(A0)                  // leitura do sensor
+  TMPtensao = (sensorValue*1.1)/1023.0;            // conversão para tensão
+  TMPcelsius = 99.62636965*TMPtensao-49.75568659;  // conversão para graus Celsius com calibração
+  return TMPcelsius;
+}
+
+// Sub-rotina do DHT22 com calibração:
+float DHT_umidadeCalibrado() {
+  DHTumidade = 1.020819677*dht.readHumidity()-2.784236918;  // calibração da medição
+  return DHTumidade;
+}
+
+// Sub-rotina do LDR:
+float LDR_luminosidade() {
+  LDRsensorValue = analogRead(A1);                      // leitura do sensor
+  LDRtensao = (LDRsensorValue/1023.0)*1.1;              // conversão para tensao
+  LDRresistencia = (tensao*184000.0)/(3.3-LDRtensao);   // conversão para resistência
+  LDRluminosidade = pow(10,5.0-log10(LDRresistencia));  // conversão para luminosidade
+  return LDRluminosidade;
+}
+
+
+////////////////// ROTINA PRINCIPAL //////////////////////
 void loop() {
   // put your main code here, to run repeatedly:
 
